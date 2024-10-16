8c474068bc3ede96e99e98f5a820bc581fdbd9be
capnemo20 <z.sujon20@outlook.com>
2024-10-16 12:10:55 +0100

    show video length time conditionally

--- scripts/video.js
+++ scripts/video.js
@@ -62,19 +62,32 @@ const displayVideos = (videos) => {
             src=${video.thumbnail}
             class="h-full w-full object-cover"
              alt="Shoes"/>
-        <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">
+             ${
+               video.others.posted_date?.length == 0
+                 ? ""
+                 : `
+                <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">
             ${video.others.posted_date}
         </span>
+                `
+             }
+        
     </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
-        <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}/>
+        <img class="w-10 h-10 rounded-full object-cover" src=${
+          video.authors[0].profile_picture
+        }/>
     </div>
     <div>
         <h2 class="font-bold">${video.title}</h2>
        <div class="flex items-center gap-2">
              <p class="text-gray-400"> ${video.authors[0].profile_name}</p>
-             ${video.authors[0].verified===true? `<img class="h-5 w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"/>`:""}
+             ${
+               video.authors[0].verified === true
+                 ? `<img class="h-5 w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"/>`
+                 : ""
+             }
        </div>
         <p></p>
     </div>
