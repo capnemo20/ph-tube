ae1973d6e8f994d677366d2c826529b2905d13f9
capnemo20 <z.sujon20@outlook.com>
2024-10-16 04:27:40 +0100

    35-4 Recap The Mechanism , Load Videos And Display Cards On The Ui

--- scripts/video.js
+++ scripts/video.js
@@ -8,23 +8,62 @@ const loadCatagories = () => {
     .then((data) => displayCatagories(data.categories))
     .catch((error) => console.log(error));
 };
+//load videos
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
 
 //create Display Catagories
 const displayCatagories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  //add data in HTML
+  categories.forEach((item) => {
+    console.log(item);
+    //create a button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
 
-    const categoryContainer = document.getElementById("categories");
-    //add data in HTML
-    categories.forEach((item)=>{
-        console.log(item);
-        //create a button
-        const button  = document.createElement("button");
-        button.classList = "btn";
-        button.innerText = item.category;
-
-        //add button to category container
-        categoryContainer.append(button)
-    })
+    //add button to category container
+    categoryContainer.append(button);
+  });
 };
 
-loadCatagories();
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact"
+    card.innerHTML = `
+    <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
 
+    videoContainer.appendChild(card);
+  });
+};
+
+//display videos
+//  const displayVideos = (videos) =>{
+//     const videoContainer  = document.getElementById("videos");
+//     videos.forEach((video)=>{
+//         console.log(video);
+//     })
+// }
+
+loadCatagories();
+loadVideos();
